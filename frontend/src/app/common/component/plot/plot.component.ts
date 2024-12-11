24ee181fcf30a38d375b5d0fe6504e1e6586f1d3
Alex Leidwein <alex.leidwein@bab.gv.at>
2024-12-11 17:28:35 +0100

    feat: auto search plot debounced

--- frontend/src/app/common/component/plot/plot.component.ts
+++ frontend/src/app/common/component/plot/plot.component.ts
@@ -1,8 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EthereumService } from '../../service/ethereum.service';
 import { Plot } from '../../type/plot.type';
 import { PlotCardComponent } from '../plot-card/plot-card.component';
@@ -24,22 +24,39 @@ import { Ownership } from '../../type/ownership.type';
   styleUrl: './plot.component.scss'
 })
 export class PlotComponent {
-  plotFormControl = new FormControl<number | null>(null);
-
   plot = signal<Plot | undefined>(undefined);
   owners = signal<Ownership[] | undefined>(undefined);
 
+  plotId = signal<number | undefined>(undefined);
+
   constructor(
     private ethereumService: EthereumService
-  ) { }
+  ) {
+    effect((onCleanup) => {
+      const plotId = this.plotId();
 
-  async getPlot() {
-    // check if plot Form value is a number
-    if (this.plotFormControl.value === undefined || this.plotFormControl.value === null) return;
+      if (plotId === undefined) return;
+      const timer = setTimeout(async () => {
+        await this.getPlot(plotId);
+      }, 200);
 
-    this.plot.set(await this.ethereumService.getPlot(this.plotFormControl.value));
-    this.owners.set(await this.ethereumService.getOwnership(this.plotFormControl.value));
-    console.log(this.plot());
-    console.log(this.owners());
+      onCleanup(() => {
+        clearTimeout(timer);
+      });
+    });
+  }
+
+  onPlotChange(event: any) {
+    if (!event.target?.value) {
+      this.plotId.set(undefined);
+      return;
+    }
+
+    this.plotId.set(event.target.value);
+  }
+
+  async getPlot(plotId: number) {
+    this.plot.set(await this.ethereumService.getPlot(plotId));
+    this.owners.set(await this.ethereumService.getOwnership(plotId));
   }
 }
